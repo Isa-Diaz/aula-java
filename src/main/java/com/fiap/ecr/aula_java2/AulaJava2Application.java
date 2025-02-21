86e5bac359625df6bdf03e1416a0b40c2ffd4c4a
isabella <isa.vdiaz20@gmail.com>
2025-02-21 20:23:24 -0300

    Aula Java - Iniciando com Spring Boot

---
+++ src/main/java/com/fiap/ecr/aula_java2/AulaJava2Application.java
@@ -0,0 +1,13 @@
+package com.fiap.ecr.aula_java2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaJava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaJava2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/ecr/aula_java2/AulaJava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ecr.aula_java2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJava2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
